9dda039fdb827206563d45e3d52752b393d91723
JuanpMedina <juampamedinadiaz2002@gmail.com>
2024-03-04 17:25:00 -0500

    Test

---
+++ test/dataLibro.js
@@ -0,0 +1,26 @@
+module.exports = [
+    {
+        "_id": "65e506de0571d1b605202e74",
+        "titulo": "Harry Potter y la piedra filosofal",
+        "autor": "J. K. Rowling",
+        "paginas": 256,
+        "editorial": "Bloomsbury Salamandra Scholastic",
+        "__v": 0
+    },
+    {
+        "_id": "65e52845b5ff4544af1693b9",
+        "titulo": "El código Da Vinci",
+        "autor": "Dan Brown",
+        "paginas": 656,
+        "editorial": "Doubleday",
+        "__v": 0
+    },
+    {
+        "_id": "65e52b44b5ff4544af1693c2",
+        "titulo": "el señor de los anillos",
+        "autor": "J. R. R. Tolkien",
+        "paginas": 1392,
+        "editorial": "ISBN",
+        "__v": 0
+    }
+]
---
+++ test/libro.test.js
@@ -0,0 +1,49 @@
+const request = require('supertest');
+const express = require('express');
+
+// Mockea el modelo de mongoose antes de importar las rutas
+const mockLibroModel = {
+    create: jest.fn(),
+    find: jest.fn(),
+    // Agrega más funciones de mongoose que quieras simular
+};
+
+jest.mock('../src/models/libro.models', () => mockLibroModel);
+
+const libroRoutes = require('../src/routes/libro.routes'); // Importa después de la configuración de los mocks
+
+const app = express();
+
+app.use(express.json());
+app.use('/apilibro', libroRoutes);
+const findResponse = require('./dataLibro')
+describe('Libro Routes', () => {
+
+    it('should get all libros', async () => {
+        mockLibroModel.find.mockResolvedValue(findResponse);
+
+        const response = await request(app).get('/apilibro/libro');
+
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toEqual(findResponse);
+    });
+
+
+    const createResponse = {
+        _id: '123456789',
+        titulo: 'Test Libro',
+        autor: 'Test Autor',
+        paginas: 200,
+        editorial: 'Test Editorial',
+    };
+
+    it('Should fail to create a Libro', async () => {
+        mockLibroModel.create.mockResolvedValue(createResponse);
+
+        const response = await request(app)
+            .post('/apilibro/libro')
+            .send({ titulo: 'Test Libro', autor: 'Test Autor', paginas: 200, editorial: 'Test Editorial' });
+
+        expect(response.statusCode).toBe(500);
+    });
+});
